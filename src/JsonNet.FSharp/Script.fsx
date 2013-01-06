ac0b37531a22fcf53c3d53b54f627346b0928b03
Lev Gorodinski <eulerfx@gmail.com>
2013-01-06 16:45:16 -0700

    Updated FSX script.

--- src/JsonNet.FSharp/Script.fsx
+++ src/JsonNet.FSharp/Script.fsx
@@ -1,7 +1,4 @@
-﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
-// for more guidance on F# programming.
-
-#I @"C:\Users\Leo\Documents\GitHub\JsonNet.FSharp\src\lib"
+﻿#I @"..\lib"
 #r "Newtonsoft.Json.dll"
 
 #load "OptionConverter.fs"
@@ -19,21 +16,21 @@ open Newtonsoft.Json
 type HaveAList = {
     Name : string
     Roles : string list
-    Prefs : string * Int64
+    Prefs : string * Int32
 }
 
 let entity = { 
     Name = "Leo G"; 
     Roles = ["admin"];
-    Prefs = ("hello",1985L)
+    Prefs = ("hello",1985)
 }
 
 let converters : JsonConverter[] = [| new ListConverter(); new TupleArrayConverter() |]
 
 let json = JsonConvert.SerializeObject(value=entity, converters=converters)
 
-printfn "The JSON is %A" json
+assert (json = """{"Name":"Leo G","Roles":["admin"],"Prefs":["hello",1985]}""")
 
 let back = JsonConvert.DeserializeObject<HaveAList>(value=json, converters=converters)
 
-printfn "The entity is %A" back
+assert (back = entity)
