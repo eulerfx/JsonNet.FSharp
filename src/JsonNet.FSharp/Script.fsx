a5b08ef151c4ebfb503535bcab016faa2cc5100c
Andreas Karlsson <andreas.karlsson3@gmail.com>
2015-05-31 12:55:00 +0200

    Update example

--- src/JsonNet.FSharp/Script.fsx
+++ src/JsonNet.FSharp/Script.fsx
@@ -2,35 +2,44 @@
 #r "Newtonsoft.Json.dll"
 
 #load "OptionConverter.fs"
-open Newtonsoft.Json.FSharp
 
 #load "TupleArrayConverter.fs"
-open Newtonsoft.Json.FSharp
 
 #load "ListConverter.fs"
+
+#load "SingleCaseUnionConverter.fs"
+
+#load "UnionEnumConverter.fs"
+
 open Newtonsoft.Json.FSharp
 
 open System
 open Newtonsoft.Json
 
-type HaveAList = {
+type Email = Email of string
+
+type Role = Admin | Sysop | User
+
+type Employee = {
     Name : string
-    Roles : string list
-    Prefs : string * Int32
+    Roles : Role list
+    Location : string * Int32
+    Email : Email
 }
 
 let entity = { 
     Name = "Leo G"; 
-    Roles = ["admin"];
-    Prefs = ("hello",1985)
+    Roles = [Admin; User];
+    Location = ("Complex 4",4)
+    Email = Email "leo.g@corporation.com"
 }
 
-let converters : JsonConverter[] = [| new ListConverter(); new TupleArrayConverter() |]
+let converters : JsonConverter[] = [| new ListConverter(); new TupleArrayConverter(); new SingleCaseUnionConverter(); new UnionEnumConverter()|]
 
 let json = JsonConvert.SerializeObject(value=entity, converters=converters)
 
-assert (json = """{"Name":"Leo G","Roles":["admin"],"Prefs":["hello",1985]}""")
+assert (json = """{"Name":"Leo G","Roles":["Admin","User"],"Location":["Complex 4",4],"Email":"leo.g@corporation.com"}""")
 
-let back = JsonConvert.DeserializeObject<HaveAList>(value=json, converters=converters)
+let back = JsonConvert.DeserializeObject<Employee>(value=json, converters=converters)
 
 assert (back = entity)
